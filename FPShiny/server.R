ff5c4d08559a586070cb23ef44c0eddc83ee55b1
Paul Askew <paul55askew@gmail.com>
2016-04-11 16:47:15 +0100

    simple update to app

--- FPShiny/server.R
+++ FPShiny/server.R
@@ -1,5 +1,11 @@
 library(shiny)
+
+# Place for code that only needs to be run once e.g. loading the data.
+# data<-readcsv("/data/fp.csv")
+# Load and run another file 
+# source("otherfile.R")
 shinyServer(
+    # this is the bit that runs when the user interacts
   function(input,output){
     }
 )
--- FPShiny/ui.R
+++ FPShiny/ui.R
@@ -2,8 +2,17 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("England Fuel Poverty Statistics"),
   sidebarPanel(
-    h3("Input Area")
-               ),
+    h3("Input Area"),
+    selectInput("var", 
+                label = "Choose a year to display",
+                choices = c("2010", "2011",
+                            "2012", "2013"),
+                selected = "2010"),
+    
+    sliderInput("range", 
+                label = "Year of interest:",
+                min = 0, max = 100, value = c(0, 100))
+  ),
   mainPanel(
     h3("Output Area")
   )
