6d5f7b8346d9247ecc45316718054f097ad57726
Paul Askew <paul55askew@gmail.com>
2016-04-12 14:10:48 +0100

    Added further preprocessing

--- FuelPovertyPreProcess.R
+++ FuelPovertyPreProcess.R
@@ -26,6 +26,21 @@ FPStatsurl<-"https://www.gov.uk/government/uploads/system/uploads/attachment_dat
 if (!file.exists("./data/consumptiondata.xlsx")){
     download.file(FPStatsurl,destfile="./data/consumptiondata.xlsx",mode="wb")
 }
-FP2011dat<-read.xlsx("./data/raw2011data2.xlsx",sheet=5,colNames=TRUE,startRow=3)
-FP2012dat<-read.xlsx("./data/raw2012data2.xlsx",sheet=7,colNames=TRUE,startRow=3)
-FP2013dat<-read.xlsx("./data/raw2013data2.xlsx",sheet=7,colNames=TRUE,startRow=3)
+FP2011dat<-read.xlsx("./data/raw2011data2.xlsx",sheet=3,colNames=TRUE,startRow=3)
+FP2012dat<-read.xlsx("./data/raw2012data2.xlsx",sheet=5,colNames=TRUE,startRow=3)
+FP2013dat<-read.xlsx("./data/raw2013data2.xlsx",sheet=5,colNames=TRUE,startRow=3)
+Cons2011dat<-read.xlsx("./data/consumptiondata.xlsx",sheet=17,colNames=TRUE,startRow=4)
+Cons2012dat<-read.xlsx("./data/consumptiondata.xlsx",sheet=18,colNames=TRUE,startRow=4)
+Cons2013dat<-read.xlsx("./data/consumptiondata.xlsx",sheet=19,colNames=TRUE,startRow=4)
+
+#Now to tidy the data up
+FP2011dat<-FP2011dat[order(FP2011dat$LA.Name),]
+FP2012dat<-FP2012dat[order(FP2012dat$LA.Name),]
+FP2013dat<-FP2013dat[order(FP2013dat$LA.Name),]
+numhouseholds<-cbind(data.frame(FP2011dat[,2],FP2011dat[,4],FP2012dat[,4],FP2013dat[,4],stringsAsFactors = FALSE))
+colnames(numhouseholds)<-c("Authority",2011,2012,2013)
+newpovmeasure<-cbind(data.frame(FP2011dat[,2],FP2011dat[,5],FP2012dat[,5],FP2013dat[,5],stringsAsFactors = FALSE))
+colnames(newpovmeasure)<-c("Authority",2011,2012,2013)
+write.csv(numhouseholds,file="./FPShiny/data/numhouseholds.csv")
+write.csv(newpovmeasure,file="./FPShiny/data/newpovmeasure.csv")
+#energy consumption columns of interest are(2,4,9,13,19,24) rows 61 to 394
