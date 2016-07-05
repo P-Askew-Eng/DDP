7f6734783ddf24945fde0c90b9f6a6a92f345b6c
Paul Askew <paul55askew@gmail.com>
2016-07-05 14:17:30 +0100

    Update helper.R
    Updated for 201 stats

--- FPShiny/helper.R
+++ FPShiny/helper.R
@@ -4,16 +4,19 @@
 houses<-read.csv("./data/numhouseholds.csv", header=T)
 rownames(houses)<-houses[,2]
 houses<-houses[,-c(1,2)]
-colnames(houses)<-c("2011","2012","2013")
+colnames(houses)<-c("2011","2012","2013","2014")
 newfp<-read.csv("./data/newpovmeasure.csv", header=T)
 rownames(newfp)<-newfp[,2]
 newfp<-newfp[,-c(1,2)]
 
 #read and tidy consumption data
-colnames(newfp)<-c("2011","2012","2013")
+colnames(newfp)<-c("2011","2012","2013","2014")
 cons2011<-read.csv("./data/Cons2011dat.csv",header=T)
 cons2012<-read.csv("./data/Cons2012dat.csv",header=T)
 cons2013<-read.csv("./data/Cons2013dat.csv",header=T)
+#cons2014<-read.csv("./data/Cons2014dat.csv",header=T)
+#rownames(cons2014)<-cons2014[,2]
+#cons2014<-cons2014[,-c(1,2)]
 rownames(cons2013)<-cons2013[,2]
 cons2013<-cons2013[,-c(1,2)]
 rownames(cons2012)<-cons2012[,2]
